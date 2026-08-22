5ce24fe5f3c736d84f67a963a7750f5c8de8906c
Lime-oss-hash <yozamono@gmail.com>
2026-08-22 20:59:54 +1200

    release: pin real sha256 hashes into brew formula + scoop manifest
    Assets built and published by CI for v0.1.0:
    - portly-x86_64-unknown-linux-gnu.tar.gz
    - portly-x86_64-unknown-linux-musl.tar.gz
    - portly-aarch64-apple-darwin.tar.gz
    - portly-x86_64-apple-darwin.tar.gz
    - portly-x86_64-pc-windows-msvc.zip (smoke-tested: --version, --once)

--- Formula/portly.rb
+++ Formula/portly.rb
@@ -7,17 +7,17 @@ class Portly < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/sambai-dev/portly/releases/download/v0.1.0/portly-aarch64-apple-darwin.tar.gz"
-      sha256 "__AARCH64_SHA256__"
+      sha256 "a3608f4783381fa8fd48b6b59f241c9970a39163a51b4b6902eb40ce7f679b69"
     else
       url "https://github.com/sambai-dev/portly/releases/download/v0.1.0/portly-x86_64-apple-darwin.tar.gz"
-      sha256 "__X86_64_MAC_SHA256__"
+      sha256 "7b303b3b48e10f747a327bea47b31c156326b01d5c58af2af25ddc163064d2a4"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sambai-dev/portly/releases/download/v0.1.0/portly-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "__LINUX_GNU_SHA256__"
+      sha256 "92ce9a2ad7bf630667ad8516da13f7ff15c6de0a1551fc1d1c01c1b295fbc599"
     end
   end
 
